d06b828f52f1f8e4f12e029597bee73a16967ccd
Iagoba Apellaniz <iagoba.apellaniz@gmail.com>
2016-10-24 16:47:41 +0200

    Standalone equations for SVG

---
+++ img/svg_src/tex/generate.sh
@@ -0,0 +1,21 @@
+#! /usr/bin/bash
+
+# Run this script to generate the equations into PDF format
+
+# PDF or project name, change this
+jobname=equations
+# String for the equation. Change it as you want
+texstr="
+  \displaystyle
+  \text{Hello this is the second equation } E=mc^2
+"
+
+# Patch to solve relative path problems
+path2this=./img/svg_src/tex
+echo $texstr > $path2this/equations.tex
+
+# Substitute relative to path2this/relative in file for the input.tex
+latexmk -pdf -shell-escape -jobname=$path2this/$jobname $path2this/main.tex
+latexmk -pdf -shell-escape -c -jobname=$path2this/$jobname $path2this/main.tex
+
+exit
