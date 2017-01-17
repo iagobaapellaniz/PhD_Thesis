604142b5f4ba5be7d42a9cc1ff1c2f79ed9f5103
Iagoba Apellaniz <iagoba.apellaniz@gmail.com>
2017-01-17 18:26:05 +0100

    Create a4 useful versions, plus no cover version

--- cvpdf_2pps.sh
+++ cvpdf_2pps.sh
@@ -1,5 +1,13 @@
 #! /bin/bash
 
-gs -o out/temp.pdf -sDEVICE=pdfwrite -sPAPERSIZE=a4 -c showpage -f out/main.pdf
+# Create a temp file with one more blank page obtained from the very same file
+# and make it 2 pages per side for draft printing
+pdfjam --paper a4paper --scale 1.0 out/main.pdf '2' out/main.pdf '-' --outfile out/temp.pdf
+pdfjam --paper a4paper --nup 2x1 --landscape out/temp.pdf --outfile out/a4-2x1-main.pdf
+rm out/temp.pdf
 
-pdfnup --nup 2x1 --paper a4paper --trim "0.5cm 0cm 0.5cm 0cm" --clip true --outfile out/2x1-a4-main.pdf out/temp.pdf
+# Create a4 main output for draft printing
+pdfjam --paper a4paper out/main.pdf '-' --outfile out/a4-main.pdf
+
+# Create an a4 main output without cover for final printing.
+pdfjam --paper letterpaper out/main.pdf '-' --outfile out/nc-main.pdf
